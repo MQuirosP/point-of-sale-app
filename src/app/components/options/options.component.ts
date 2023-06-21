804fa1cbcea134f471cb3c3aac84c532354d35bb
MQuirosP <mquirosp78@gmail.com>
2023-06-21 02:09:22 -0600

    modified:   .firebase/hosting.ZGlzdFxmcm9udGVuZA.cache 	modified:   src/app/components/options/options.component.html 	modified:   src/app/components/options/options.component.ts 	modified:   src/app/components/product-list/product-list.component.ts 	modified:   src/styles.css

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -1,9 +1,11 @@
+import { map } from 'rxjs';
 import { style } from '@angular/animations';
 import { OptionsService } from './../../services/optionsService';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { fadeAnimation } from 'src/app/fadeAnimation';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-options',
@@ -21,20 +23,25 @@ export class OptionsComponent implements OnInit {
 
   backendUrl = `${environment.apiUrl}`
 
-  providersList: any[] = [];
-  customersList: any[] = [];
-  searchTermProvider: any[] = [];
-  searchTermCustomer: any[] = [];
+  providers: any[] = [];
+  filteredProviders: any[] = [];
+  searchTermProvider: string = '';
+  customers: any[] = [];
+  filteredCustomers: any[] = [];
+  searchTermCustomer: string = '';
 
   constructor(
     private optionService: OptionsService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private http: HttpClient,
+    private changeDetectorRef: ChangeDetectorRef,
   ) {}
 
   ngOnInit() {
     this.optionService.getRegisterStatus().subscribe((status: boolean) => {
       this.activateRegister = status;
     });
+    this.getProviderList();
   }
 
   saveRegisterStatus() {
@@ -52,10 +59,8 @@ export class OptionsComponent implements OnInit {
     });
   }
 
-  filterProviders() {}
 
-  filterCustomers() {}
-
+  
   openProviderMainteinanceModal() {
     this.providerModal.nativeElement.classList.toggle('show');
     this.providerModal.nativeElement.style.display = 'block';
@@ -78,5 +83,107 @@ export class OptionsComponent implements OnInit {
 
   getProviderList() {
 
+    if ( this.providers.length > 0 ) {
+      return;
+    }
+    
+    this.http.get(`${this.backendUrl}providers`).subscribe({
+      next: (response: any) => {
+        if (
+          response.success &&
+          response.message.providers.length > 0
+        ) {
+          this.providers = response.message.providers.map((provider: any) => {
+            return { ...provider, showIcons: false}
+          });
+        }
+        this.filteredProviders = [...this.providers];
+        // console.log(this.providers);
+      },
+      error: (error) => {
+        this.toastr.error('Error al obtener proveedores', error)
+      }
+    })
+  }
+
+  filterProviders() {
+    if ( this.searchTermProvider === '' ) {
+      this.filteredProviders = [...this.providers];
+      return;
+    }
+    const searchTermNormalized = this.searchTermProvider
+    .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+
+    this.filteredProviders = this.providers.filter((provider: any) => {
+      const providerNameNormalized = provider.name
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+        
+        const providerDniNormalized = provider.provider_dni
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+
+      const searchTermLower = searchTermNormalized.toLowerCase();
+
+      return (
+        providerNameNormalized.toLowerCase().includes(searchTermLower) ||
+        providerDniNormalized.toLowerCase().includes(searchTermLower)
+        );
+    });
+    this.changeDetectorRef.detectChanges();
+  }
+
+  getCustomerList() {
+
+    if ( this.providers.length > 0 ) {
+      return;
+    }
+    
+    this.http.get(`${this.backendUrl}customers`).subscribe({
+      next: (response: any) => {
+        if (
+          response.success &&
+          response.message.providers.length > 0
+        ) {
+          this.customers = response.message.customers.map((customer: any) => {
+            return { ...customer, showIcons: false}
+          });
+        }
+        this.filteredCustomers = [...this.customers];
+        // console.log(this.providers);
+      },
+      error: (error) => {
+        this.toastr.error('Error al obtener clientes', error)
+      }
+    })
+  }
+
+  filterCustomers() {
+    if ( this.searchTermCustomer === '' ) {
+      this.filteredProviders = [...this.providers];
+      return;
+    }
+    const searchTermNormalized = this.searchTermCustomer
+    .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+
+    this.filteredCustomers = this.customers.filter((customer: any) => {
+      const providerNameNormalized = customer.name
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+        
+        const providerDniNormalized = customer.customer_dni
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '');
+
+      const searchTermLower = searchTermNormalized.toLowerCase();
+
+      return (
+        providerNameNormalized.toLowerCase().includes(searchTermLower) ||
+        providerDniNormalized.toLowerCase().includes(searchTermLower)
+        );
+    });
+    this.changeDetectorRef.detectChanges();
   }
 }
