3c86bc8d2af25e1e82eb64d895a432f54959e9f9
MQuirosP <mquirosp78@gmail.com>
2023-08-04 16:36:12 -0600

    modified:   .firebase/hosting.ZGlzdFxmcm9udGVuZA.cache 	modified:   src/app/fadeAnimation.ts

--- src/app/fadeAnimation.ts
+++ src/app/fadeAnimation.ts
@@ -3,9 +3,9 @@ import { trigger, transition, style, animate } from '@angular/animations';
 export const fadeAnimation = trigger('fadeAnimation', [
   transition(':enter', [
     style({ opacity: 0 }),
-    animate('300ms', style({ opacity: 1 }))
+    animate('200ms', style({ opacity: 1 }))
   ]),
   transition(':leave', [
-    animate('300ms', style({ opacity: 0 }))
+    animate('200ms', style({ opacity: 0 }))
   ])
 ]);
