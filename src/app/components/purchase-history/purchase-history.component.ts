a4c3a25797fbd23fa014fa3d94b9d0656c09a484
MQuirosP <mquirosp78@gmail.com>
2023-08-15 18:31:26 -0600

    modified:   .firebase/hosting.ZGlzdFx2ZXJkdWxlcmlhLWFwcA.cache 	modified:   src/app/components/front-panel/front-panel.component.html 	modified:   src/app/components/purchase-history/purchase-history.component.html 	modified:   src/app/components/purchase-history/purchase-history.component.ts 	modified:   src/styles.css

--- src/app/components/purchase-history/purchase-history.component.ts
+++ src/app/components/purchase-history/purchase-history.component.ts
@@ -72,42 +72,42 @@ interface Product {
   styleUrls: ['./purchase-history.component.css'],
   animations: [
     fadeAnimation,
-    // trigger('slideInOut', [
-    //   state(
-    //     'in',
-    //     style({
-    //       transform: 'translateX(0)',
-    //       opacity: 1,
-    //     })
-    //   ),
-    //   state(
-    //     'out',
-    //     style({
-    //       transform: 'translateX(-100%)',
-    //       opacity: 0,
-    //     })
-    //   ),
-    //   transition('in => out', animate('200ms ease-out')),
-    //   transition('out => in', animate('200ms ease-in')),
-    // ]),
-    // trigger('slideOut', [
-    //   state(
-    //     'in',
-    //     style({
-    //       transform: 'translateX(0)',
-    //       opacity: 1,
-    //     })
-    //   ),
-    //   state(
-    //     'out',
-    //     style({
-    //       transform: 'translateX(100%)',
-    //       opacity: 0,
-    //     })
-    //   ),
-    //   transition('in => out', animate('200ms ease-out')),
-    //   transition('out => in', animate('200ms ease-in')),
-    // ]),
+    trigger('slideInOut', [
+      state(
+        'in',
+        style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'out',
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+        })
+      ),
+      transition('in => out', animate('200ms ease-out')),
+      transition('out => in', animate('200ms ease-in')),
+    ]),
+    trigger('slideOut', [
+      state(
+        'in',
+        style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })
+      ),
+      state(
+        'out',
+        style({
+          transform: 'translateX(100%)',
+          opacity: 0,
+        })
+      ),
+      transition('in => out', animate('200ms ease-out')),
+      transition('out => in', animate('200ms ease-in')),
+    ]),
   ],
 })
 export class PurchaseHistoryComponent {
