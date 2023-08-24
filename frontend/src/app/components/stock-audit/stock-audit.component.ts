514cbe7af3d13bc0215ad3a9a280641c24b00fde
MQuirosP <mquirosp78@gmail.com>
2023-08-24 12:47:32 -0600

    new file:   frontend/src/app/components/stock-audit/stock-audit.component.css 	new file:   frontend/src/app/components/stock-audit/stock-audit.component.html 	new file:   frontend/src/app/components/stock-audit/stock-audit.component.spec.ts 	new file:   frontend/src/app/components/stock-audit/stock-audit.component.ts

---
+++ frontend/src/app/components/stock-audit/stock-audit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-audit',
+  templateUrl: './stock-audit.component.html',
+  styleUrls: ['./stock-audit.component.css']
+})
+export class StockAuditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
