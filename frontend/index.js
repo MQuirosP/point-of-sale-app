01d2652c5691318dd80d0c96b463ab53f12f8c74
MQuirosP <mquirosp78@gmail.com>
2023-08-26 17:14:36 -0600

    modified:   frontend/index.js 	modified:   frontend/package-lock.json 	modified:   frontend/package.json

--- frontend/index.js
+++ frontend/index.js
@@ -13,6 +13,7 @@ function createWindow() {
     },
   });
 
+  // Cargar la URL del archivo index.html empaquetado
   mainWindow.loadURL(
     url.format({
       pathname: path.join(__dirname, 'dist', 'verduleria-app', 'index.html'),
@@ -21,19 +22,23 @@ function createWindow() {
     })
   );
 
+  // Manejar el evento de cierre de la ventana
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
+// Cuando Electron esté listo, crea la ventana
 app.on('ready', createWindow);
 
+// Manejar el evento de todas las ventanas cerradas
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+// Manejar el evento de activación (para macOS)
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
