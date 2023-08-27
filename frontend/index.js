a130c1328eeb83da4436bbe79d12a5117d7d67b8
MQuirosP <mquirosp78@gmail.com>
2023-08-27 17:19:30 -0600

    modified:   frontend/index.js 	modified:   frontend/package-lock.json 	modified:   frontend/package.json

--- frontend/index.js
+++ frontend/index.js
@@ -1,42 +1,34 @@
-const {
-  app,
-  BrowserWindow
-} = require('electron')
-const url = require("url");
-const path = require("path");
-let appWindow
-function initWindow() {
-  appWindow = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-  // Electron Build Path
-  appWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/verduleria-app/index.html`),
-      protocol: "file:",
-      slashes: true
-    })
-  );
-  // Initialize the DevTools.
-  appWindow.webContents.openDevTools()
-  appWindow.on('closed', function () {
-    appWindow = null
-  })
+const { app, BrowserWindow } = require("electron");
+
+let appWin;
+
+createWindow = () => {
+    appWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: "Angular and Electron",
+        resizable: false,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    });
+    
+    appWin.loadURL(`file://${__dirname}/dist/verduleria-app/index.html`);
+
+    appWin.setMenu(null);
+
+    appWin.webContents.openDevTools();
+
+    appWin.on("closed", () => {
+        appWin = null;
+    });
 }
-app.on('ready', initWindow)
-// Close when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS specific close process
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-app.on('activate', function () {
-  if (win === null) {
-    initWindow()
-  }
-})
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+});
