b71b7a3ce2308624c8f2ad2da7f452d2729d5c42
MQuirosP <mquirosp78@gmail.com>
2023-08-27 17:26:29 -0600

    deleted:    angular.json 	modified:   frontend/index.js 	modified:   frontend/package-lock.json 	modified:   frontend/package.json

--- frontend/index.js
+++ frontend/index.js
@@ -1,34 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const nw = require('nw');
 
-let appWin;
-
-createWindow = () => {
-    appWin = new BrowserWindow({
-        width: 800,
-        height: 600,
-        title: "Angular and Electron",
-        resizable: false,
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true
-        }
-    });
-    
-    appWin.loadURL(`file://${__dirname}/dist/verduleria-app/index.html`);
-
-    appWin.setMenu(null);
-
-    appWin.webContents.openDevTools();
-
-    appWin.on("closed", () => {
-        appWin = null;
-    });
-}
-
-app.on("ready", createWindow);
-
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-      app.quit();
-    }
-});
+// Lanzar la ventana principal
+nw.Window.open('index.html', {}, function(win) {});
