dfe54ffc23c23a71b9b158ba2a36fe73de868207
MQuirosP <mquirosp78@gmail.com>
2023-08-26 19:08:26 -0600

    modified:   frontend/index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
 let mainWindow;
 
@@ -13,10 +14,14 @@ function createWindow() {
     },
   });
 
-  // Cargar la URL del archivo index.html empaquetado
+  // Obtener el nombre de archivo con hash de index.html
+  const indexPath = path.join(__dirname, 'dist', 'verduleria-app');
+  const indexHtmlName = fs.readdirSync(indexPath).find(file => file.startsWith('index.') && file.endsWith('.html'));
+
+  // Cargar la URL del archivo index.html empaquetado con el hash actual
   mainWindow.loadURL(
     url.format({
-      pathname: path.join(__dirname, 'dist', 'verduleria-app', 'index.html'),
+      pathname: path.join(indexPath, indexHtmlName),
       protocol: 'file:',
       slashes: true,
     })
