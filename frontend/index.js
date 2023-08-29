4876a51f3180b163bcef40621d3d72190a3f9a95
MQuirosP <mquirosp78@gmail.com>
2023-08-29 10:26:52 -0600

    modified:   frontend/electron-builder.json 	modified:   frontend/index.js 	modified:   frontend/package.json 	modified:   package.json

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 let win;
 
@@ -7,39 +7,40 @@ function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 800,
+    title: "Mi Punto de Venta",
+    icon: path.join(__dirname, "favicon.ico"),
     webPreferences: {
-      nodeIntegration: true, // Permite la integración de Node.js en el contexto de la página web
-      webSecurity: true, // Deshabilita la seguridad del navegador (¡solo para desarrollo!)
+      nodeIntegration: true,
+      webSecurity: true,
     },
+    autoHideMenuBar: true,
   });
 
-  // Obtener la ruta absoluta del directorio donde se encuentra este script (main.js)
   const appPath = app.getAppPath();
 
-  // Construir la ruta completa al index.html de la aplicación Angular
-  const indexPath = path.join(__dirname, 'dist', 'verduleria-app', 'index.html');
+  const indexPath = path.join(
+    __dirname,
+    "dist",
+    "verduleria-app",
+    "index.html"
+  );
 
-  // Cargar el index.html en la ventana de Electron
   win.loadFile(indexPath);
 
-  // Escuchar el evento de cierre de la ventana
-  win.on('closed', () => {
+  win.on("closed", () => {
     win = null;
   });
 }
 
-// Cuando la aplicación esté lista, crea la ventana
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-// Cuando todas las ventanas estén cerradas, cierra la aplicación (excepto en macOS)
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-// Cuando la aplicación se active (haga clic en el icono en el dock en macOS), crea la ventana si no existe
-app.on('activate', () => {
+app.on("activate", () => {
   if (win === null) {
     createWindow();
   }
