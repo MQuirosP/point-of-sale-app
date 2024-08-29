12a509e018e0f65e26343bf21adca81b140db6af
Mario Quirós <115672555+MQuirosP@users.noreply.github.com>
2024-08-29 17:27:38 -0600

    Documentation

--- frontend/src/app/services/scanner.service.ts
+++ frontend/src/app/services/scanner.service.ts
@@ -1,6 +1,10 @@
 import * as Quagga from 'quagga';
 import { Injectable } from '@angular/core';
 
+/**
+ * Servicio para manejar el escaneo de códigos de barras utilizando la cámara del dispositivo.
+ * Incluye métodos para iniciar y detener el escaneo, así como para procesar los resultados.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +14,10 @@ export class ScannerService {
 
   private audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
 
+  // CONFIGURACIÓN DEL BEEP
+  /**
+   * Reproduce un sonido de "beep" cuando se detecta un código de barras.
+   */
   playBeepSound() {
     const oscillator = this.createOscillator();
     const gain = this.createGain();
@@ -21,7 +29,42 @@ export class ScannerService {
     oscillator.stop(this.audioContext.currentTime + 0.1);
   }
 
-  startScanning(video: HTMLVideoElement, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D | null, onDetectedCallback: (code: string) => void) {
+  /**
+   * Crea un oscilador para reproducir el sonido de "beep".
+   * @returns El oscilador creado.
+   */
+  private createOscillator(): OscillatorNode {
+    const oscillator = this.audioContext.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.setValueAtTime(440, this.audioContext.currentTime);
+    return oscillator;
+  }
+
+  /**
+   * Crea un nodo de ganancia para ajustar el volumen del sonido.
+   * @returns El nodo de ganancia creado.
+   */
+  private createGain(): GainNode {
+    const gain = this.audioContext.createGain();
+    gain.gain.setValueAtTime(0.9, this.audioContext.currentTime);
+    return gain;
+  }
+
+  // IMPLEMENTACIÓN DEL ESCANER POR CAMARA
+  /**
+   * Inicia el proceso de escaneo configurando el flujo de video y el análisis de Quagga.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   * @param canvas - Elemento de canvas HTML sobre el cual se dibujan los cuadros de detección.
+   * @param ctx - Contexto del canvas donde se dibujan los cuadros y líneas.
+   * @param onDetectedCallback - Función de callback que se llama cuando se detecta un código de barras.
+   */
+  startScanning(
+    video: HTMLVideoElement, 
+    canvas: HTMLCanvasElement, 
+    ctx: CanvasRenderingContext2D | null, 
+    onDetectedCallback: (code: string) => void) 
+    {
+
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
@@ -34,12 +77,22 @@ export class ScannerService {
     }
   }
 
+  /**
+   * Detiene el escaneo, detiene el flujo de video y limpia el canvas.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   * @param canvas - Elemento de canvas HTML sobre el cual se dibujan los cuadros de detección.
+   */
   stopScanning(video: HTMLVideoElement, canvas: HTMLCanvasElement) {
     this.stopVideoStream(video);
     this.stopQuagga();
     this.clearCanvas(canvas);
   }
 
+  /**
+   * Configura el flujo de video a partir de un stream de medios y reproduce el video.
+   * @param stream - Stream de medios del video.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   */
   private setupVideoStream(stream: MediaStream, video: HTMLVideoElement) {
     video.srcObject = stream;
     video.onloadedmetadata = () => {
@@ -48,6 +101,10 @@ export class ScannerService {
     };
   }
 
+  /**
+   * Actualiza las dimensiones y el estilo del canvas según el tamaño del video.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   */
   private updateCanvasDimensions(video: HTMLVideoElement) {
     const canvas = document.getElementById('overlay') as HTMLCanvasElement;
     if (canvas) {
@@ -59,6 +116,12 @@ export class ScannerService {
     }
   }
 
+  /**
+   * Calcula el desplazamiento necesario para alinear el canvas con el video.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   * @param canvas - Elemento de canvas HTML sobre el cual se dibujan los cuadros de detección.
+   * @returns Un objeto con los desplazamientos en X y Y.
+   */
   private calculateOffset(video: HTMLVideoElement, canvas: HTMLCanvasElement) {
     const videoWidth = video.videoWidth;
     const videoHeight = video.videoHeight;
@@ -72,7 +135,13 @@ export class ScannerService {
     return { offsetX, offsetY };
   }
 
-
+  /**
+   * Inicializa Quagga para el escaneo de códigos de barras con las configuraciones especificadas.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   * @param canvas - Elemento de canvas HTML sobre el cual se dibujan los cuadros de detección.
+   * @param ctx - Contexto del canvas donde se dibujan los cuadros y líneas.
+   * @param onDetectedCallback - Función de callback que se llama cuando se detecta un código de barras.
+   */
   private initializeQuagga(
     video: HTMLVideoElement, 
     canvas: HTMLCanvasElement, 
@@ -121,6 +190,13 @@ export class ScannerService {
     this.setupQuaggaEvents(video, canvas, ctx, onDetectedCallback); 
   }
 
+  /**
+   * Configura los eventos de Quagga para el procesamiento y la detección de códigos de barras.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   * @param canvas - Elemento de canvas HTML sobre el cual se dibujan los cuadros de detección.
+   * @param ctx - Contexto del canvas donde se dibujan los cuadros y líneas.
+   * @param onDetectedCallback - Función de callback que se llama cuando se detecta un código de barras.
+   */
   private setupQuaggaEvents(
     video: HTMLVideoElement, 
     canvas: HTMLCanvasElement, 
@@ -142,7 +218,13 @@ export class ScannerService {
     });
   }
 
-
+  /**
+   * Procesa el resultado de Quagga y dibuja los cuadros de detección y líneas en el canvas.
+   * @param result - Resultado del procesamiento de Quagga.
+   * @param ctx - Contexto del canvas donde se dibujan los cuadros y líneas.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   * @param canvas - Elemento de canvas HTML sobre el cual se dibujan los cuadros de detección.
+   */
   private processQuaggaResult(
     result: any, 
     ctx: CanvasRenderingContext2D | null, 
@@ -174,6 +256,15 @@ export class ScannerService {
     }
   }
 
+  /**
+   * Dibuja una ruta ajustada en el canvas teniendo en cuenta el desplazamiento.
+   * @param path - Array de coordenadas que forman la ruta.
+   * @param offsetX - Desplazamiento en X.
+   * @param offsetY - Desplazamiento en Y.
+   * @param ctx - Contexto del canvas donde se dibujan los cuadros y líneas.
+   * @param color - Color de la línea.
+   * @param lineWidth - Ancho de la línea.
+   */
   private drawAdjustedPath(
     path: any[], 
     offsetX: number, 
@@ -182,7 +273,7 @@ export class ScannerService {
     color: string, 
     lineWidth: number) 
     {
-      
+
     ctx.beginPath();
     ctx.lineWidth = lineWidth;
     ctx.strokeStyle = color;
@@ -198,6 +289,10 @@ export class ScannerService {
     ctx.stroke();
   }
 
+  /**
+   * Detiene el flujo de video asociado al elemento de video.
+   * @param video - Elemento de video HTML que muestra la transmisión de la cámara.
+   */
   private stopVideoStream(video: HTMLVideoElement) {
     if (video.srcObject) {
       const stream = video.srcObject as MediaStream;
@@ -206,11 +301,18 @@ export class ScannerService {
     }
   }
 
+   /**
+   * Detiene Quagga y desactiva el evento de detección.
+   */
   private stopQuagga() {
     Quagga.stop();
     Quagga.offDetected();
   }
 
+  /**
+   * Limpia el contenido del canvas.
+   * @param canvas - Elemento de canvas HTML sobre el cual se dibujan los cuadros de detección.
+   */
   private clearCanvas(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext('2d');
     if (ctx) {
@@ -218,20 +320,13 @@ export class ScannerService {
     }
   }
 
+  /**
+   * Maneja errores al acceder a la cámara.
+   * @param error - Objeto de error.
+   */
   private handleCameraError(error: any) {
     console.error('Error al acceder a la cámara:', error);
   }
 
-  private createOscillator(): OscillatorNode {
-    const oscillator = this.audioContext.createOscillator();
-    oscillator.type = 'square';
-    oscillator.frequency.setValueAtTime(440, this.audioContext.currentTime);
-    return oscillator;
-  }
-
-  private createGain(): GainNode {
-    const gain = this.audioContext.createGain();
-    gain.gain.setValueAtTime(0.9, this.audioContext.currentTime);
-    return gain;
-  }
+  
 }
